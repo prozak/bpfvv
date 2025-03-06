ddd12ea1a7ba78e6d11e39e335f76affca441b48
Ihor Solodrai <ihor.solodrai@linux.dev>
2025-03-05 16:13:31 -0800

    Implement simple pattern-matched highlighting
    A line can be selected. If selected line is an instruction writing to
    a register, previous lines that may have written to the source
    register are highlighted.
    
    This is a very primitive analysis of the instruction stream, just to
    have something to play with in the UI.
    
    Signed-off-by: Ihor Solodrai <ihor.solodrai@linux.dev>

--- app.ts
+++ app.ts
@@ -1,7 +1,22 @@
+type InsnArgs = {
+    src: string;
+    dst: string;
+    src_reg: string;
+    dst_reg: string;
+}
+
+type ParsedInsnLine = {
+    idx: number;
+    hex: string;
+    insn: InsnArgs;
+    comment: string;
+}
+
 type ParsedLine = {
     idx: number; // index of the line in the input file
     offset: number; // byte offset in the input file pointing to the start of the line
     raw: string;
+    insnLine?: ParsedInsnLine;
 }
 
 type AppState = {
@@ -13,6 +28,9 @@ type AppState = {
     lineHeight: number;
 
     topLineIdx: number; // index of the first line in the visible area
+    selectedLine: number;
+    focusReg: string | null;
+    maxParsedLineIdx: number;
 }
 
 const createApp = () => {
@@ -36,15 +54,100 @@ const createApp = () => {
         visibleLines: 50,
         totalHeight: 0,
         lineHeight: 16,
-
+        selectedLine: 13,
+        focusReg: null,
+        maxParsedLineIdx: 0,
         topLineIdx: 0,
     };
 
-    const updateLoadStatus = async (state: AppState): Promise<void> => {
-        const lastLine = state.lines[state.lines.length - 1];
-        const percentage = lastLine.offset / state.fileBlob.size * 100;
-        loadStatus.innerHTML = `Loaded ${percentage.toFixed(0)}% (${lastLine.idx + 1} lines)`;
+    const parseInsn = (rawLine: string): ParsedInsnLine => {
+
+        const regex = /([0-9]+): \(([0-9a-f]+)\) (.*)\s+; (.*)/;
+        const match = rawLine.match(regex);
+        if (!match)
+            return null;
+
+        console.log(match[3].trim());
+
+        const insn_str = match[3].trim();
+        if (insn_str.startsWith('call ')) {
+            return {
+                idx: parseInt(match[1], 10),
+                hex: match[2],
+                insn: {
+                    src: insn_str,
+                    dst: insn_str,
+                    src_reg: null,
+                    dst_reg: 'r0',
+                },
+                comment: match[4].trim(),
+            };
+        }
+
+        const insn_match = insn_str.split(' = ');
+
+        let dst = null;
+        let src = null;
+        let dst_reg = null;
+        let src_reg = null;
+        if (insn_match) {
+            dst = insn_match[0];
+            const dst_match = dst?.match(/\br[0-9]\b/);
+            dst_reg = dst_match ? dst_match[0] : null;
+            if (insn_match.length > 1) {
+                src = insn_match[1];
+                const src_match = src?.match(/\br[0-9]\b/);
+                src_reg = src_match ? src_match[0] : null;
+            }
+        }
+
+        return {
+            idx: parseInt(match[1], 10),
+            hex: match[2],
+            insn: {
+                src: insn_match[1],
+                dst: insn_match[0],
+                src_reg: src_reg,
+                dst_reg: dst_reg,
+            },
+            comment: match[4].trim(),
+        };
+    }
+
+    const handleLineClick = async (e: MouseEvent) => {
+        const clickedLine = (e.target as HTMLElement).closest('.log-line');
+        if (!clickedLine) return;
+
+        const lineIndex = parseInt(clickedLine.getAttribute('line-index') || '0', 10);
+        state.selectedLine = lineIndex;
+
+        const line = state.lines[lineIndex];
+        state.focusReg = line?.insnLine?.insn.src_reg;
+
+        updateView(state);
     };
+
+    const isFocusLine = (line: ParsedLine): boolean => {
+        if (!state.focusReg)
+            return false;
+        const dst_reg = line?.insnLine?.insn?.dst_reg;
+        if (!dst_reg || !dst_reg.startsWith('r'))
+            return false;
+        if (dst_reg === state.focusReg && line?.idx < state.selectedLine)
+            return true;
+        return false;
+    }
+
+    const formatLogLine = (line: ParsedLine, idx: number): string => {
+        let highlightClass = 'normal';
+        if (idx === state.selectedLine)
+            highlightClass = 'selected';
+        else if (isFocusLine(line))
+            highlightClass = 'focus';
+        else if (!line?.insnLine)
+            highlightClass = 'ignorable';
+        return `<div class="log-line ${highlightClass}" line-index="${idx}">${escapeHtml(line.raw)}</div>`
+    }
 
     // Load and parse the file into memory from the beggining to the end
     const loadInputFile = async (state: AppState): Promise<void> => {
@@ -66,6 +169,7 @@ const createApp = () => {
                     idx: idx,
                     offset: offset,
                     raw: rawLine,
+                    insnLine: parseInsn(rawLine),
                 };
                 state.lines.push(parsedLine);
                 offset += rawLine.length + 1;
@@ -73,6 +177,14 @@ const createApp = () => {
             });
             updateLoadStatus(state);
         }
+    };
+
+    const updateLoadStatus = async (state: AppState): Promise<void> => {
+        if (state.lines.length === 0)
+            return;
+        const lastLine = state.lines[state.lines.length - 1];
+        const percentage = lastLine.offset / state.fileBlob.size * 100;
+        loadStatus.innerHTML = `Loaded ${percentage.toFixed(0)}% (${lastLine.idx + 1} lines)`;
     };
 
     const updateLineNumbers = async (startLine: number, count: number): Promise<void> => {
@@ -87,10 +199,10 @@ const createApp = () => {
         for (let idx = state.topLineIdx; idx < state.topLineIdx + state.visibleLines; idx++) {
             const line = state.lines[idx];
             if (line)
-                lines.push(line.raw);
+                lines.push(formatLogLine(line, idx));
         }
-        contentLines.innerHTML = lines.join('\n');
-    };
+        contentLines.innerHTML = lines.join('');
+    }
 
     const updateView = async (state: AppState): Promise<void> => {
         updateLineNumbers(state.topLineIdx, state.visibleLines);
@@ -140,6 +252,12 @@ const createApp = () => {
         updateView(state);
     };
 
+    const escapeHtml = (text: string): string => {
+        const div = document.createElement('div');
+        div.textContent = text;
+        return div.innerHTML;
+    };
+
     const processFile = async (file: File): Promise<void> => {
         state.topLineIdx = 0;
         state.lines = [];
@@ -181,7 +299,7 @@ const createApp = () => {
     gotoLineInput.addEventListener('input', gotoLine);
     gotoStartButton.addEventListener('click', gotoStart);
     gotoEndButton.addEventListener('click', gotoEnd);
-
+    contentLines.addEventListener('click', handleLineClick);
     updateView(state);
 
     // Return cleanup function
@@ -194,6 +312,7 @@ const createApp = () => {
         gotoStartButton.removeEventListener('click', gotoStart);
         gotoLineButton.removeEventListener('click', gotoLine);
         gotoEndButton.removeEventListener('click', gotoEnd);
+        contentLines.removeEventListener('click', handleLineClick);
     };
 };
 
